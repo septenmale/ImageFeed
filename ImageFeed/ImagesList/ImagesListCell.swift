e3aa0352261101f2a3770d5a6cca10783da6de4d
Viktor <zavhorodnii.viktor@icloud.com>
2024-10-09 20:21:52 +0200

    small fixes

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -4,7 +4,7 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentiifier = "ImagesListCell"
     
-    @IBOutlet var likeButton: UIButton!
-    @IBOutlet var cellImage: UIImageView!
-    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet  var likeButton: UIButton!
+    @IBOutlet  var cellImage: UIImageView!
+    @IBOutlet  var dateLabel: UILabel!
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -4,7 +4,7 @@ class ImagesListViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
-    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private let photosName = Array(0..<20).map{ "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -15,7 +15,6 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
