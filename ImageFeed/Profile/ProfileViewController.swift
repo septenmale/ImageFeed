5b77c0b1bc3648085b6ff4bca24f707122cc09a7
Viktor <zavhorodnii.viktor@icloud.com>
2024-10-11 09:57:48 +0200

    small fixes, ready to go

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -6,10 +6,10 @@ final class ProfileViewController: UIViewController {
     @IBOutlet private var nameLabel: UILabel!
     @IBOutlet private var loginNameLabel: UILabel!
     @IBOutlet private var descriptionLabel: UILabel!
-
+    
     @IBOutlet private var logoutButton: UIButton!
-
-    @IBAction private func didTapLogoutButton() {
+    
+    @IBAction private func didTapLogoutButtom() {
     }
     
 }
