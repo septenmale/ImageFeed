3717d9d37c835af37b0ecbe344d371ad81db3343
Viktor <zavhorodnii.viktor@icloud.com>
2024-10-16 16:24:38 +0200

    save commit, scroll effect added no bugs

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -3,17 +3,36 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
+            
         }
     }
     
+    
+    
     @IBOutlet  var imageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
-    }
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        }
     @IBAction func didTapBackButton(_ sender: UIButton) { dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        
+        return imageView
+        
     }
 }
