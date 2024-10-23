3c0eb8d47a14accb58598c1bcf7f5e7af1cbf72b
Viktor <zavhorodnii.viktor@icloud.com>
2024-10-23 12:37:58 +0200

    URl Components done, URL Request created, no errors

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     private let showWebViewSequeIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureBackButton()
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")?.withRenderingMode(.alwaysOriginal)
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")?.withRenderingMode(.alwaysOriginal)
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "ypBlack")
+    }
+    
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -1,5 +1,38 @@
 import UIKit
+import WebKit
 
 final class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthoriseURLString) else {
+            print("Error: Failed to create urlComponents. Check unsplashAuthoriseURLString.")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            print("Error: Failed to create URL from the provided URl components")
+            return
+        }
+        
+        let reguest = URLRequest(url: url)
+        webView.load(reguest)
+    }
+    
+    enum WebViewConstants {
+        static let unsplashAuthoriseURLString = "https://unsplash.com/oauth/authorize"
+    }
 }
