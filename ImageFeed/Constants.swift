207861304560a899c0f9cf726f513955bd3cd11a
Viktor <zavhorodnii.viktor@icloud.com>
2024-10-27 11:28:26 +0100

    func tokenReguest done, seems like no errors

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -3,7 +3,7 @@ import Foundation
 enum Constants {
     static let accessKey = "E6WhIRRibsG0wUYDenBjGj0DSbomdiW_V6FhgXncOcU"
     static let secretKey = "gThBwqHmIKZxaQog0MsT2n25qGiEtYoa37tOtvh9cFA"
-    static let redirectURI = "667493"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")
 }
