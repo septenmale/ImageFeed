8519042ba9d49ad06f944bb1dbc6b400eb4cca5f
Viktor <zavhorodnii.viktor@icloud.com>
2024-11-04 17:50:28 +0100

    lesson 3/4 finished, no errors

--- ImageFeed/NetworkService/OAuth2/OAuth2Service.swift
+++ ImageFeed/NetworkService/OAuth2/OAuth2Service.swift
@@ -1,9 +1,18 @@
 import UIKit
 import WebKit
 
+enum AuthServiceError: Error {
+    case invalidRequest
+}
+
 final class OAuth2Service {
     
     static let shared = OAuth2Service()
+    
+    private let urlSession: URLSession = URLSession.shared
+    
+    private var lastTask: URLSessionTask?
+    private var lastCode: String?
     
     private init() {}
     
@@ -35,31 +44,58 @@ final class OAuth2Service {
     }
     
     func fetchOAuthToken(with code: String, handler: @escaping (Result<String, Error>) -> Void) {
-        guard let request = authTokenRequest(code: code) else {
+        assert(Thread.isMainThread) // проверяем что код вып-ся из гл-го потока
+        guard lastCode != code else {
+            handler(.failure(AuthServiceError.invalidRequest))
+            return
+        }
+//        if lastTask != nil { // пр-м вып-ся ли щас пост запрос
+//            if lastCode != code { // пр-м что в запросе который щас в процессе такое же как в аргументе
+//                lastTask?.cancel() // отменяем запрос: code не актуальный
+//            } else {
+//                handler(.failure(AuthServiceError.invalidRequest))
+//                return
+//            }
+//        } else {
+//            if lastCode == code { // запросов нет, но токен для данного code получен
+//                handler(.failure(AuthServiceError.invalidRequest))
+//                return
+//            }
+//        }
+        lastTask?.cancel() // если решусь вернуться - удалить
+        lastCode = code
+        
+        
+        guard let request = authTokenRequest(code: code) else { // Создаём запрос на получение Auth Token c даным кодом
             print("Error: Failed to create an authorization request for code \(code)")
             handler(.failure(NetworkError.urlSessionError))
             return
         }
         
         let task = URLSession.shared.data(for: request) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let decoder = JSONDecoder()
-                    let response = try decoder.decode(OAuthTokenResponseBody.self, from: data)
-                    handler(.success(response.accessToken))
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let response = try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                        handler(.success(response.accessToken))
+                        
+                        OAuth2TokenStorage.shared.token = response.accessToken
+                    } catch {
+                        print("Error while decoding: \(error.localizedDescription)")
+                        handler(.failure(error))
+                    }
                     
-                    OAuth2TokenStorage.shared.token = response.accessToken
-                } catch {
-                    print("Error while decoding: \(error.localizedDescription)")
+                case . failure(let error):
+                    print("Network error: \(error.localizedDescription)")
                     handler(.failure(error))
+                    self.lastTask = nil
+                    self.lastCode = nil
                 }
-                
-            case . failure(let error):
-                print("Network error: \(error.localizedDescription)")
-                handler(.failure(error))
             }
         }
+        self.lastTask = task
         task.resume()
     }
     
