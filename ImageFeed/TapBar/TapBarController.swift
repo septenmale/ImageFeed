14088de8bf4a121db84f251f8b109b470f1d3109
Viktor <zavhorodnii.viktor@icloud.com>
2024-11-18 10:28:34 +0100

    VC connections transfered ftom SB to code

---
+++ ImageFeed/TapBar/TapBarController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class TapBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+        
+        let profileViewController = storyboard.instantiateViewController(
+            withIdentifier: "ProfileViewController"
+        )
+        
+        self.viewControllers = [imagesListViewController, profileViewController]
+        
+    }
+    
+}
