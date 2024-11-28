a8a175b6ed567e75e4407ab21fd3561d3f8e7e5d
Viktor <zavhorodnii.viktor@icloud.com>
2024-11-28 11:12:59 +0100

    refractored, no errors

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -3,39 +3,39 @@ import UIKit
 import Kingfisher
 
 final class ImagesListCell: UITableViewCell {
-    static let reuseIdentiifier = "ImagesListCell"
+    static let reuseIdentifier = "ImagesListCell"
     
     @IBOutlet  var likeButton: UIButton!
     @IBOutlet  var cellImage: UIImageView!
     @IBOutlet  var dateLabel: UILabel!
     
-    var imageDownloadTask: DownloadTask?
-    
-    // Переменная для хранения задачи на загрузку изображения
+    /// Prepares a reusable cell for reuse by the table view's delegate
     override func prepareForReuse() {
         super.prepareForReuse()
         
-        imageDownloadTask?.cancel()
-        imageDownloadTask = nil
-        
+        cellImage.kf.cancelDownloadTask()
         cellImage.image = UIImage(named: "placeholder")
+        
     }
-        // TODO: в самой ячейке в prepareForReuse нужно отменить задачу на загрузку фото.
 }
 
 extension ImagesListCell {
-    func configure(with url: URL) {
-        // Настроим индикатор загрузки
-        cellImage.kf.indicatorType = .activity
+    /// prepares image for a next cell
+    /// - Parameters:
+    ///   - url: image URL
+    ///   - indexPath: image's Index Path
+    ///   - tableView: current Table View
+    func configureImage(with url: URL, for indexPath: IndexPath, in tableView: UITableView) {
+        setupLoadingIndicator()
         
-        // Настроим загрузку изображения с использованием Kingfisher
-        imageDownloadTask = cellImage.kf.setImage(with: url, placeholder: UIImage(named: "placeholder")) { result in
-            switch result {
-            case .success(let value):
-                print("[ImageListViewController]: Image successfully downloaded from \(value.source.url?.absoluteString ?? "")")
-            case .failure(let error):
-                print("[ImageListViewController]: Error while downloading image: \(error.localizedDescription)")
-            }
-        }
+        cellImage.kf.setImage(with: url,
+                              placeholder: UIImage(named: "placeholder") ?? UIImage()
+        ) { result in }
+        //TODO: Check if possible try to insert tableView.reloadRows(at: [indexPath], with: .automatic)
     }
+    
+    private func setupLoadingIndicator() {
+        cellImage.kf.indicatorType = .activity
+    }
+    
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -3,15 +3,13 @@ import ProgressHUD
 import Kingfisher
 
 final class ImagesListViewController: UIViewController {
-    var photos: [Photo] = [] // Массив, который будет хранить данные для таблицы
+    var photos: [Photo] = []
     private var imageListServiceObserver: NSObjectProtocol?
     
     private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private let imageListService = ImageListService()
     
     @IBOutlet private var tableView: UITableView!
-    
-//    private let photosName = Array(0..<20).map{ "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -57,8 +55,6 @@ final class ImagesListViewController: UIViewController {
                 return
             }
             
-//            let image = UIImage(named: photosName[indexPath.row])
-            
             let photo = photos[indexPath.row]
             if let url = URL(string: photo.largeImageURL) {
                 viewController.imageURL = url
@@ -66,10 +62,15 @@ final class ImagesListViewController: UIViewController {
         } else {
             super.prepare(for: segue, sender: sender)
         }
+        
     }
     
     private func loadFirstPage() {
         imageListService.fetchPhotosNextPage { _ in }
+    }
+    
+    private func isLastRow(indexPath: IndexPath) -> Bool {
+        indexPath.row == photos.count - 1
     }
     
 }
@@ -79,17 +80,6 @@ extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         let photo = photos[indexPath.row]
         
-        if let url = URL(string: photo.thumbImageURL) {
-                    // Загрузка изображения с помощью Kingfisher
-                    cell.cellImage.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"))
-                }
-        
-//        guard let image = UIImage(named: photosName[indexPath.row]) else {
-//            return
-//        }
-//        cell.cellImage.image = image
-        
-//        cell.dateLabel.text = dateFormatter.string(from: Date())
         if let createdAt = photo.createdAt {
             cell.dateLabel.text = dateFormatter.string(from: createdAt)
         } else {
@@ -110,37 +100,23 @@ extension ImagesListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let photo = photos[indexPath.row]
-            // новое
-//        guard let url = URL(string: photo.thumbImageURL) else {
-//            return 0
-//        }
-        // новое
-//        let imageView = UIImageView()
-//        let image = imageView.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"))
-        
-//        guard let image = UIImage(named: photosName[indexPath.row]) else {
-//            return 0
-//        }
         
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-//        let imageWidth = image.size.width
         let imageWidth = photo.size.width
         let scale = imageViewWidth / imageWidth
-//        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
         let cellHeight = photo.size.height * scale + imageInsets.top + imageInsets.bottom
-//        return cellHeight
         return cellHeight
     }
-    // перенес сюда с отдельного расширения
+    
     func tableView(_ tableView: UITableView,
                    willDisplay cell: UITableViewCell,
                    forRowAt indexPath: IndexPath
-    ) {
-        // поменять в будущем на isLastRow
-        if indexPath.row + 1 == photos.count {
-                    imageListService.fetchPhotosNextPage { _ in }
-                }
+    ){
+        
+        if isLastRow(indexPath: indexPath) {
+            imageListService.fetchPhotosNextPage { _ in }
+        }
     }
     
 }
@@ -148,26 +124,23 @@ extension ImagesListViewController: UITableViewDelegate {
 extension ImagesListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photos.count // исправлено
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentiifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
         
-        // Получаем текущую фотографию из массива photos
         let photo = photos[indexPath.row]
         
-        // Формируем URL для загрузки изображения
         if let url = URL(string: photo.thumbImageURL) {
-            // Настроим ячейку с помощью URL изображения
-            imageListCell.configure(with: url)
+            
+            imageListCell.configureImage(with: url, for: indexPath, in: tableView)
         }
         
-        // Дополнительные настройки ячейки
         configCell(for: imageListCell, with: indexPath)
         
         return imageListCell
@@ -175,24 +148,25 @@ extension ImagesListViewController: UITableViewDataSource {
     
 }
 
-    extension ImagesListViewController {
-        func updateTableViewAnimated() {
-            let oldCount = photos.count
-            
-            let newCount = imageListService.photos.count
-            
-            photos = imageListService.photos
-            
-            if oldCount != newCount {
-                var indexPaths: [IndexPath] = []
-                for i in oldCount..<newCount {
-                    indexPaths.append(IndexPath(row: i, section: 0))
-                }
-                
-                tableView.performBatchUpdates {
-                    tableView.insertRows(at: indexPaths, with: .automatic)
-                }  completion: { _ in }
+extension ImagesListViewController {
+    ///update Table View  if new photos loaded
+    func updateTableViewAnimated() {
+        let oldCount = photos.count
+        
+        let newCount = imageListService.photos.count
+        
+        photos = imageListService.photos
+        
+        if oldCount != newCount {
+            var indexPaths: [IndexPath] = []
+            for i in oldCount..<newCount {
+                indexPaths.append(IndexPath(row: i, section: 0))
             }
+            
+            tableView.performBatchUpdates {
+                tableView.insertRows(at: indexPaths, with: .automatic)
+            }  completion: { _ in }
         }
     }
+}
 
